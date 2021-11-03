a70cf92c43ff0137e2f78517b8036a0b2c028193
AlfonsoOC <80928378+AlfonsoOC@users.noreply.github.com>
2021-11-02 22:27:30 -0400

    Anatoations and some library adds

--- Chapter01/recipe_2.R
+++ Chapter01/recipe_2.R
@@ -1,5 +1,5 @@
 
-count_dataframe <- readr::read_tsv(file.path(getwd(), "datasets", "ch1", "modencodefly_count_table.txt" ))
+count_dataframe <- readr::read_tsv(file.path(getwd(),"/..","datasets", "ch1", "modencodefly_count_table.txt" ))
 
 genes <- count_dataframe[['gene']]
 count_dataframe[['gene']] <- NULL
@@ -8,30 +8,36 @@ count_matrix <- as.matrix(count_dataframe)
 rownames(count_matrix) <- genes
 
 
-pheno_data <- readr::read_table2(file.path(getwd(), "datasets", "ch1", "modencodefly_phenodata.txt"))
+pheno_data <- readr::read_table2(file.path(getwd(),"/..", "datasets", "ch1", "modencodefly_phenodata.txt")) #here they use readtable2 from readr function, nut sure why
 
 experiments_of_interest <- c("L1Larvae", "L2Larvae")
 columns_of_interest <- which( pheno_data[['stage']] %in% experiments_of_interest ) #gives the numeric indices of the above experiments in the matrix
 
+library("dplyr")
+library("magrittr")
 grouping <- pheno_data %>% 
   dplyr::filter(stage %in% experiments_of_interest )
 
-counts_of_interest <-  count_matrix[,columns_of_interest]
+counts_of_interest <-  count_matrix[,columns_of_interest] #using the indexing to extract the columns we want to analize
 
 
 library("DESeq2")
+# convert the matrix in DESeqDataSet object
 dds <- DESeqDataSetFromMatrix(countData = counts_of_interest,
                               colData = grouping,
                               design = ~ stage)
 
-
+#Performing the Data Analysis
 dds <- DESeq(dds)
+
+#For Results: This shows the mean counts, the log2 fold change between samples for a gene, the standard error of the log2 fold change, the Wald statistic, and the raw and adjusted P value.
+#The padjcolumn for adjusted P values is the one most commonly used for concluding about significance.
 res <- results(dds, contrast=c("stage","L2Larvae","L1Larvae"))
 
 
 
 library(SummarizedExperiment)
-load(file.path(getwd(), "datasets/ch1/modencodefly_eset.RData"))
+load(file.path(getwd(), "/../datasets/ch1/modencodefly_eset.RData"))
 summ_exp <- makeSummarizedExperimentFromExpressionSet(modencodefly.eset)
 ddsSE <- DESeqDataSet(summ_exp, design= ~ stage)
 
