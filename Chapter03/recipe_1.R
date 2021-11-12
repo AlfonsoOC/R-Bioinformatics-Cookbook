30070d68505582714c0d68d51382c4c03d04f8c1
AlfonsoOC <80928378+AlfonsoOC@users.noreply.github.com>
2021-11-11 23:53:52 -0500

    Annotations added

--- Chapter03/recipe_1.R
+++ Chapter03/recipe_1.R
@@ -1,21 +1,38 @@
+#Finding DNA motifs with universalmotif
+
+##1 First, load the libraries and a motif of interest:
+
 library(universalmotif)
 library(Biostrings)
 
 
-
-motif <- read_matrix(file.path(getwd(), "datasets", "ch3","simple_motif.txt"))
+#this is the description of the motif 
+motif <- read_matrix(file.path(getwd(), "GitHub","R-Bioinformatics-Cookbook","datasets", "ch3","simple_motif.txt"))
 view_motifs(motif)
 
-sequences <- readDNAStringSet(file.path(getwd(), "datasets", "ch3", "promoters.fa"))
+##2 Then, load in sequences to scan with the motif:
+
+sequences <- readDNAStringSet(file.path(getwd(), "GitHub","R-Bioinformatics-Cookbook", "datasets", "ch3", "promoters.fa"))
+
+## 3 Perform a scan of the sequences:
+#The scan_sequences() function searches each of the sequences and reports where it finds motifs—check out the motif_hits object to see where they are.
 
 motif_hits <- scan_sequences(motif, sequences = sequences)
 head(motif_hits)
 
-motif_info <- enrich_motifs(motif, sequences, shuffle.k = 3, verbose = 0, progress = FALSE, RC = TRUE)
+## 4 Calculate whether the motif is enriched in the sequences:
+#It searches the sequences to find likely instances of motifs and counts them, performing Fisher's exact test to compare the frequencies of motifs in our set of sequences with their frequencies in an automatically generated background set.
+motif_info <- enrich_motifs(motif, sequences, shuffle.k = 3, verbose = 0, RC = TRUE) #progress = FALSE
 motif_info
 
-meme_path = "/SET/TO/YOUR/MEME/PATH"
+## 5 Run MEME to find novel motifs:
+#To find novel motifs, we run the external software MEME
+meme_path = "~/Documents/GitHub/R-Bioinformatics-Cookbook/SET/TO/YOUR/MEME/PATH"
 meme_run <- run_meme(sequences, bin = meme_path, output = "meme_out", overwrite.dir = TRUE)
 
+#the meme function return a result out of R environment, then we have to import that result to R
 motifs <- read_meme("meme_out/meme.txt")
 view_motifs(motifs)
+
+#Loading in motifs from pre-existing databases such as JASPAR and TRANSFAC is very easy with universalmotif as there are straightforward replacements for the read_matrix() function. Look at the following functions to load in motifs from various formats: read_cisbp(), read_homer(), read_jaspar(), read_matrix(), read_meme(), read_motifs(), and read_uniprobe().
+
--- Chapter03/recipe_2.R
+++ Chapter03/recipe_2.R
@@ -1,3 +1,6 @@
+##Finding protein domains with PFAM and bio3d
+
+
 print("Arabidopsis")
 library(org.At.tair.db)
 columns(org.At.tair.db)
@@ -11,14 +14,21 @@ library(org.Hs.eg.db)
 
 
 ##inspect the keytype available - different from columns in that these are the things we can actually use to access
+#check if "PFAM" is available in here
 keytypes(org.Hs.eg.db)
 
 ##make a vector of keys (here ensembl IDs) to work with, could come from other source, just pick 3 genes
-k <- head(keys(org.Hs.eg.db, keytype = "ENSEMBL"), n = 3 )
+#we can use other keys has: ALIAS, EVIDENCE, GENENAME... insetad of ENSEMBL 
+k <- head(keys(org.Hs.eg.db, keytype = "ENSEMBL"), n = 3 ) # here we are selecting the first 3 genes
+#We also can do this wit k:
+#to check the names of the genes keys(org.Hs.eg.db, keytype = "ENSEMBL")
+#then select 3 or more:
+#k <- c("ENSG00000138435", "ENSG00000226651", "ENSG00000135902")
 
 #select the data and get the annotation
-result <- select(org.Hs.eg.db, keys = k, keytype="ENSEMBL", columns = c("PFAM"))
-result
+result <- select(org.Hs.eg.db, keys = k, keytype="ENSEMBL", columns = c("PFAM")) #if we change "ENSEMBL in top we have to changed in here too 
+result # this give us the PFAM IDs
+
 ## now get the descriptions
 
 library(PFAM.db)
@@ -28,14 +38,17 @@ descriptions <- PFAMDE
 ##get all keys IE all PFAM IDs that exist in the database
 all_ids <- mappedkeys(descriptions)
 
-#get all the descriptions for the mappings
+#get all the descriptions for the mappings as a data frame
 
 id_description_mapping <- as.data.frame(descriptions[all_ids])
 
-dplyr::left_join(result, id_description_mapping, by = c("PFAM" = "ac") )
+dplyr::left_join(result, id_description_mapping, by = c("PFAM" = "ac") ) #we join the descriptions with the PFAM IDs
 
+
+##There's more...
+##procedure. If it doesn't show up, then as an alternative procedure, it is possible to run PFAM and make the annotations yourself.
 library(bio3d)
-sequence <- read.fasta(file.path(getwd(), "datasets", "ch3", "ecoli_hsp.fa") )
+sequence <- read.fasta(file.path(getwd(),"GitHub/R-Bioinformatics-Cookbook/", "datasets", "ch3", "ecoli_hsp.fa") )
 # run pfamseq on protein
 result <- hmmer(sequence, type="hmmscan", db="pfam")
 result$hit.tbl
