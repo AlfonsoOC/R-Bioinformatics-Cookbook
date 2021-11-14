9504f0e58628565f5ee5e4c087d87ecf637ac6ae
AlfonsoOC <80928378+AlfonsoOC@users.noreply.github.com>
2021-11-14 12:12:39 -0500

    annotations

--- Chapter03/recipe_4.R
+++ Chapter03/recipe_4.R
@@ -1,13 +1,20 @@
+##Performing multiple alignments of genes or proteins
+
+## 1 Load libraries and sequences
 library(msa)
-seqs <- readAAStringSet(file.path(getwd(), "datasets", "ch3", "hglobin.fa"))
+seqs <- readAAStringSet(file.path(getwd(),"GitHub/R-Bioinformatics-Cookbook/", "datasets", "ch3", "hglobin.fa"))
 seqs
 
+## 2 Perform the multiple sequence aligment
 alignment <- msa(seqs, method = "ClustalOmega")
 alignment
 
+#view the result using the next code:
+#crea el archivo para visualizar el alineamiento  
 msaPrettyPrint(alignment, output="pdf", showNames="left",
                showLogo="none", askForOverwrite=FALSE, verbose=FALSE, file="whole_align.pdf")
 
+#crea el archivo para visualizar una region del alineamiento  
 msaPrettyPrint(alignment, c(10,30), output="pdf", showNames="left",
                file = "zoomed_align.pdf", showLogo="top", askForOverwrite=FALSE, verbose=FALSE)
 
--- Chapter03/recipe_5.R
+++ Chapter03/recipe_5.R
@@ -1,19 +1,27 @@
+##Aligning genomic length sequences with DECIPHER
+
+## 1 Load in the libraries and genome sequences:
 library(DECIPHER)
-long_seqs <- readDNAStringSet(file.path(getwd(), "datasets", "ch3", "plastid_genomes.fa"))
+long_seqs <- readDNAStringSet(file.path(getwd(),"GitHub/R-Bioinformatics-Cookbook/", "datasets", "ch3", "plastid_genomes.fa"))
 long_seqs
+
+## 2 Prepare the sequences in a local database:
 ##make a sequence db on disk
 Seqs2DB(long_seqs, "XStringSet", "long_db", names(long_seqs))
 
+## 3 Find the blocks of synteny:
 ## Find Syntenic bloocks
 synteny <- FindSynteny("long_db")
 
 ##view syntenic blocks
 pairs(synteny)
-plot(synteny)
+plot(synteny) ## 4 Plot the syntetic blocks
 
-##make an actual alignment
+
+## 5 make an actual alignment
 alignment <- AlignSynteny(synteny, "long_db")
 
+## 6 And save the pairwise alignments one-by-one:
 ##Is a structure holding all aligned syntenic blocks for each pair of sequences here genomes 1 and 2
 blocks <- unlist(alignment[[1]])
 
